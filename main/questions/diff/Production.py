5a220910baee2e8b2c424f277d2674cefa147388
HuyLafa <anhhuy0706@gmail.com>
2016-09-29 19:52:33 -0400

    Remove setlatex functions in diff

--- main/questions/diff/Production.py
+++ main/questions/diff/Production.py
@@ -15,12 +15,10 @@ def plus( func1, func2 ):
 	if func1.constant() and func2.constant():
 		# output function is also a constant
 		function = Function( str( float(str1) + float(str2) ), True )
-		function.setlatex( function.getStringFunc() )
 	else:
 		str1 = "(" + str1 + ")"
 		str2 = "(" + str2 + ")"
 		function = Function( str1 + " + " + str2, False, False )
-		function.setlatex( func1.getlatex() + " + " + func2.getlatex() )
 
 	return function
 
@@ -28,20 +26,14 @@ def plus( func1, func2 ):
 def minus( func1, func2 ):
 	str1 = func1.getStringFunc()
 	str2 = func2.getStringFunc()
-	latex1 = func1.getlatex()
-	latex2 = func2.getlatex()
 
 	if func1.constant() and func2.constant():
 		# output function is also a constant
 		function = Function( str( float(str1) - float(str2) ), True )
-		function.setlatex( function.getStringFunc() )
 	else:
 		str1 = "(" + str1 + ")"
-		latex1 = "(" + latex1 + ")"
 		str2 = "(" + str2 + ")"
-		latex2 = "(" + latex2 + ")"
 		function = Function( str1 + " - " + str2, False, False )
-		function.setlatex( latex1 + " - " + latex2 )
 
 	return function
 
@@ -49,14 +41,11 @@ def minus( func1, func2 ):
 def times( func1, func2 ):
 	str1 = func1.getStringFunc()
 	str2 = func2.getStringFunc()
-	latex1 = func1.getlatex()
-	latex2 = func2.getlatex()
 
 	if func1.constant():
 		# output function is also a constant
 		if func2.constant():
 			function = Function( str( float(str1) * float(str2) ), True )
-			function.setlatex( function.getStringFunc() )
 			return function
 		# if func1 = 1, output is just func2
 		elif float( func1.getStringFunc() ) == 1:
@@ -68,12 +57,9 @@ def times( func1, func2 ):
 
 	# if no function is constant, append the two
 	str1 = "(" + str1 + ")"
-	latex1 = "(" + latex1 + ")"
 	str2 = "(" + str2 + ")"
-	latex2 = "(" + latex2 + ")"
 
 	function = Function( str1 + " * " + str2, False, False )
-	function.setlatex( latex1 + " \cdot " + latex2 )
 	return function
 
 
@@ -85,7 +71,6 @@ def divide( func1, func2 ):
 		# output function is also a constant
 		if func1.constant():
 			function = Function( str( float(str1) / float(str2) ), True )
-			function.setlatex( function.getStringFunc() )
 			return function
 		elif float( func2.getStringFunc() ) == 1:
 			return func1
@@ -93,7 +78,6 @@ def divide( func1, func2 ):
 	str1 = "(" + str1 + ")"
 	str2 = "(" + str2 + ")"
 	function = Function( str1 + " / " + str2, False, False )
-	function.setlatex( "\\dfrac{" + func1.getlatex() + "}{" + func2.getlatex() + "}" )
 	return function
 
 
@@ -101,20 +85,14 @@ def powerConst( func1, func2 ):
 	assert func2.constant()
 	str1 = "(" + func1.getStringFunc() + ")"
 	str2 = func2.getStringFunc()
-	latex1 = "(" + func1.getlatex() + ")"
-	latex2 = func2.getlatex() # no need for ( ) in power
 	function = Function( str1 + "**" + str2 , False, False )
-	function.setlatex( latex1 + "^{" + latex2 + "}")
 	return function
 
 
 def power( func1, func2 ):
 	str1 = "(" + func1.getStringFunc() + ")"
 	str2 = "(" + func2.getStringFunc() + ")"
-	latex1 = "(" + func1.getlatex() + ")"
-	latex2 = func2.getlatex() # no need for ( ) in power
 	function = Function( str1 + "**" + str2 , False, False )
-	function.setlatex( latex1 + "^{" + latex2 + "}")
 	return function
 
 
@@ -122,7 +100,6 @@ def compose( func1, func2 ):
 	if func1.constant():
 		return func1
 	function = Function( func1.getStringFunc().replace("x&", "(" + func2.getStringFunc() + ")"), False, False)
-	function.setlatex( func1.getlatex().replace("x&", "(" + func2.getlatex() + ")") )
 	return function
 
 ########## ELEMENTARY FUNCTIONS ######################
@@ -135,13 +112,11 @@ def compose( func1, func2 ):
 #	dContent: string representation of the derivative
 #	dLatex: latex representation of the derivative
 def buildFunction( 
-	content, latex, dContent, dLatex,  
+	content, dContent,
 	dIsConstant = False, dIsElementary = False,
 	isConstant = False, isElementary = True,):
 	func = Function( content, isConstant, isElementary )
-	func.setlatex( latex )
 	derivative = Function( dContent, dIsConstant, dIsElementary )
-	derivative.setlatex( dLatex )
 	func.setDerivative( derivative )
 	return func
 
@@ -149,116 +124,101 @@ def buildFunction(
 def const( number = None ):
 	if number is None:
 		number = randint(1, 5)
-	return buildFunction( str(number), str(number), "0", "0", True, True, True, True )
+	return buildFunction( str(number), "0", True, True, True, True )
 
 
 def linear():
 	randomConstant = str(randint(1, 10))
 
 	if randomConstant == "1":
-		return buildFunction( "x&", "x&", randomConstant, randomConstant, True, True )
+		return buildFunction( "x&", randomConstant, True, True )
 	else:
-		return buildFunction( 
-			"(" + randomConstant + "*x&)", randomConstant + "x&", 
-			randomConstant, randomConstant, True, True )
+		return buildFunction( "(" + randomConstant + "*x&)", randomConstant, True, True )
 
 
 def sin():
-	return buildFunction( "sin(x&)", "\\sin x&", "cos(x&)", "\\cos x&", False, True )
+	return buildFunction( "sin(x&)", "cos(x&)", False, True )
 
 
 def cos():
-	return buildFunction( "cos(x&)", "\\cos x&", "-sin(x&)", "-\\sin x&" )
+	return buildFunction( "cos(x&)", "-sin(x&)" )
 
 
 def tan():
-	return buildFunction( "tan(x&)", "\\tan x&", "sec(x&)**2", "(\\sec x&)^2" )
+	return buildFunction( "tan(x&)", "sec(x&)**2" )
 
 
 def sec():
-	return buildFunction( "sec(x&)", "\\sec x&", "sec(x&) * tan(x&)", "\\sec x& \cdot \\tan x&" )
+	return buildFunction( "sec(x&)", "sec(x&) * tan(x&)" )
 
 
 def csc():
-	return buildFunction( "csc(x&)", "\\csc x&", "-(csc(x&) * cot(x&))", "-\csc x& \\cdot \\cot x&" )
+	return buildFunction( "csc(x&)", "-(csc(x&) * cot(x&))" )
 
 
 def cot():
-	return buildFunction( "cot(x&)", "\\cot x&", "-( csc(x&) )**2", "-(\csc x&)^2" )
+	return buildFunction( "cot(x&)", "-( csc(x&) )**2" )
 
 
 def exp():
-	return buildFunction( "exp(x&)", "e^{x&}", "exp(x&)", "e^{x&}", False, True )
+	return buildFunction( "exp(x&)", "exp(x&)", False, True )
 
 
 def ln():
-	return buildFunction( "ln(x&)", "\\ln x&", "1 / x&", "\\dfrac{ 1 }{x&}" )
+	return buildFunction( "ln(x&)", "1 / x&" )
 
 
 def sqrt():
-	return buildFunction( 
-		"sqrt(x&)", "\\sqrt{x&}", "1/(2 * sqrt(x&))", "\\dfrac{1}{2\\sqrt{x&}}" )
+	return buildFunction( "sqrt(x&)", "1/(2 * sqrt(x&))" )
 
 
 def arcsin():
-	return buildFunction( 
-		"asin(x&)", "\\arcsin(x&)", "1/sqrt(1 - (x&)**2)", "\\dfrac{1}{\\sqrt{1-(x&)^2}" )
+	return buildFunction( "asin(x&)", "1/sqrt(1 - (x&)**2)" )
 
 
 def arccos():
-	return buildFunction(
-		"acos(x&)", "\\arccos(x&)", "-1/sqrt(1-(x&)**2)", "\\dfrac{-1}{\\sqrt{1-(x&)^2}" )
+	return buildFunction("acos(x&)", "-1/sqrt(1-(x&)**2)" )
 
 
 def arctan():
 	return buildFunction(
-		"atan(x&)", "\\arctan(x&)", "1/((x&)**2+1)", "\\dfrac{1}{(x&)^2 + 1}" )
+		"atan(x&)", "1/((x&)**2+1)" )
 
 
 def arccot():
-	return buildFunction("acot(x&)", "\\arccot(x&)", "-1/((x&)**2+1)", "\\dfrac{1}{(x&)^2+1}" )
+	return buildFunction("acot(x&)", "-1/((x&)**2+1)" )
 
 
 def arcsec():
-	return buildFunction( 
-		"asec(x&)", "\\text{arcsec}(x&)", 
-		"1/((x&)**2 * sqrt(1 - 1/((x&)**2)))",
-		"\\dfrac{1}{(x&)^2 \\sqrt{1 - \\frac{1}{(x&)^2}}}" )
+	return buildFunction( "asec(x&)", "1/((x&)**2 * sqrt(1 - 1/((x&)**2)))" )
 
 
 def arccsc():
-	return buildFunction(
-		"arccsc(x&)", "\\text{arccsc}(x&)",
-		"-1/((x&)**2 * sqrt(1 - 1/((x&)**2)))",
-		"\\dfrac{1}{(x&)^2 \\sqrt{1 - \\frac{1}{(x&)^2}}}" )
+	return buildFunction( "arccsc(x&)", "-1/((x&)**2 * sqrt(1 - 1/((x&)**2)))" )
 
 
 def sinh():
-	return buildFunction( "sinh(x&)", "\\sinh(x&)", "cosh(x&)", "\\cosh(x&)", False, True )
+	return buildFunction( "sinh(x&)", "cosh(x&)", False, True )
 
 
 def cosh():
-	return buildFunction( "cosh(x&)", "\\cosh(x&)", "sinh(x&)", "\\sinh(x&)", False, True )
+	return buildFunction( "cosh(x&)", "sinh(x&)", False, True )
 
 
 def tanh():
-	return buildFunction( "tanh(x&)", "\\tanh(x&)", "(sech(x&))**2", "\\text{sech}(x&)^2" )
+	return buildFunction( "tanh(x&)", "(sech(x&))**2" )
 
 
 def coth():
-	return buildFunction( "coth(x&)", "\\coth(x&)", "-csch(x&)**2", "-\\text{csch}((x&)^2" )
+	return buildFunction( "coth(x&)", "-csch(x&)**2" )
 
 
 def sech():
-	return buildFunction( 
-		"sech(x&)", "\\text{sech}(x&)", 
-		"-tanh(x&) * sech(x&)", "-\\tanh(x&) \cdot \\text{sech}(x&)" )
+	return buildFunction( "sech(x&)", "-tanh(x&) * sech(x&)" )
 
 
 def csch():
-	return Function( 
-		"csch(x&)", "\\text{csch}(x&)", 
-		"-coth(x&) * csch(x&)", "-\\coth(x&) \cdot \\text{csch}(x&)" )
+	return Function( "csch(x&)","-coth(x&) * csch(x&)" )
 
 
 class Production:
@@ -275,7 +235,6 @@ class Production:
 		upto = 0.0
 		for choice in self.elemFunctions.keys():
 			w = self.elemFunctions[choice]
-			#prfloat(w)
 			if upto + w >= r:
 				return choice
 			upto += w
@@ -283,7 +242,9 @@ class Production:
 
 
 	@classmethod
-	def getDerivative(self, productionRule, func1, func2, func1D, func2D ):
+	def getDerivative(self, productionRule, func1, func2 ):
+		func1D = func1.getDerivative()
+		func2D = func2.getDerivative()
 		if productionRule == "plus":
 			return plus( func1D, func2D )
 		if productionRule == "minus":
@@ -314,7 +275,7 @@ class Production:
 					func1D
 				)
 			)
-		#prfloat("no match")
+		assert False, "Unrecognized production rule"
 
 
 	@classmethod
@@ -356,7 +317,7 @@ class Production:
 		power : 8
 	}
 
-	# printing name for debugging only
+	# printing name for debugging
 	nameMap = {
 		plus: "plus",
 		minus: "minus",
