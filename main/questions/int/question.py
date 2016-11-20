3dbce7ccef2b58e44d922c5986f34bf874710164
wassgha <wassgha@gmail.com>
2016-11-20 15:11:19 -0500

    Corrected path bug

--- main/questions/int/question.py
+++ main/questions/int/question.py
@@ -41,7 +41,7 @@ class Question(object):
 			session['int']['questionNum'] = session['questionNum'] + 1
 			session.save()
 		questionFileName = "../../question_factory/int/generated_questions/difficulty" + str(self.difficulty) + "_" + str(session['questionNum']) + ".question"
-		with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), questionFileName), 'rb') as questionFile:
+		with open(os.path.join(os.path.abspath(os.path.dirname(__name__)), questionFileName), 'rb') as questionFile:
 			self.question = pickle.load(questionFile)
 
 	def preprocessLat2Sym(self, string):
