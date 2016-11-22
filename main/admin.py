4a1491b108715f7adf39a2fab93210ef447cb9e2
wassgha <wassgha@gmail.com>
2016-11-22 14:19:38 -0500

    Added huy and dahl users to admin

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import Course, Topic, Exercise, Attempt
 
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('user', 'exercise', 'question', 'answer', 'correct', 'submit_date')
+
+
 admin.site.register(Course)
 admin.site.register(Topic)
 admin.site.register(Exercise)
-admin.site.register(Attempt)
+admin.site.register(Attempt, AttemptAdmin)
