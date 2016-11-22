8629736f453a79676b0f350d3dca8fea4a30a16c
wassgha <wassgha@gmail.com>
2016-11-22 15:23:18 -0500

    Show progress only for active exercises

--- main/templatetags/progress.py
+++ main/templatetags/progress.py
@@ -7,13 +7,12 @@ register = template.Library()
 @register.assignment_tag
 def exercise_progress(user, exercise):
 	num_correct = Attempt.objects.filter(user = user, exercise=exercise, correct=True).count()
-	print "num_correct = " + str(num_correct) + " question_count = " + str(exercise.question_count) + " percent = " + str((100*num_correct)/exercise.question_count)
 	return 0 if num_correct is None else (100*num_correct)/exercise.question_count
 
 @register.assignment_tag
 def topic_progress(user, topic):
-	topic_question_count = Exercise.objects.filter(topic = topic).aggregate(Sum('question_count'))['question_count__sum']
+	topic_question_count = Exercise.objects.filter(topic = topic, exercise_active = True).aggregate(Sum('question_count'))['question_count__sum']
 	if topic_question_count is None:
 		return 0
-	num_correct = Attempt.objects.filter(user = user, exercise__in=topic.exercise_set.all(), correct=True).count()
+	num_correct = Attempt.objects.filter(user = user, exercise__in=topic.exercise_set.filter(exercise_active = True), correct=True).count()
 	return 0 if num_correct is None else (100*num_correct)/topic_question_count
